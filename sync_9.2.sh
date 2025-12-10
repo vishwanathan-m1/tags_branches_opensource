cdb4e66d40876f5ee81fa27c08170473f7323320
vishwanathan-m <vishwanathan.m@pnp-hcl.com>
2025-12-10 13:47:51 +0530

    Update sync_9.2.sh

--- sync_9.2.sh
+++ sync_9.2.sh
@@ -1,6 +1,6 @@
 git fetch external main
 git checkout sync/9.2
-git merge main --allow-unrelated-histories
+git merge main -m "merge internal main to external sync/9.2" --allow-unrelated-histories
 git fetch -u external sync/9.2
 git pull external sync/9.2
 git push -u external sync/9.2
