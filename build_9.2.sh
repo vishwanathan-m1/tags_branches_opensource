28a8d97f981114765da66be1d747ed896124ae60
vishwanathan-m <vishwanathan.m@pnp-hcl.com>
2025-12-10 16:50:40 +0530

    Update build_9.2.sh

--- build_9.2.sh
+++ build_9.2.sh
@@ -5,5 +5,5 @@ git fetch --tags --force
 git tag -f v9.2
 git push -f origin v9.2
 git checkout tags/v9.2
-mike deploy --push --update-aliases v9.2 latest -t V9.2
+mike deploy --push v9.2 -t V9.2
 git checkout main
